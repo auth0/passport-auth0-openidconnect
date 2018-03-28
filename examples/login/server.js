68cf66b5dce5b25c91ab02bc792f08252a8c7ddb
Samuel Judson <samuel.judson@auth0.com>
2018-03-28 14:18:32 -0500

    Move example to universal login.

--- examples/login/server.js
+++ examples/login/server.js
@@ -94,15 +94,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes.
 
-app.get('/', function (req, res) {
-  res.render('index', { 
-    env: {
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-      AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL
-    } 
-  });
-});
+app.get('/', passport.authenticate('auth0-oidc'));
 
 app.get('/logout', function (req, res) {
   req.logout();
